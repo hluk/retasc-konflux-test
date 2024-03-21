d5bd1cecd69c725722887b45c6459a6e6fd5535d
Lukas Holecek <hluk@email.cz>
2024-03-21 09:18:14 +0100

    Increase test coverage

--- api/api_test.go
+++ api/api_test.go
@@ -12,7 +12,7 @@ func TestHello(t *testing.T) {
 	router := SetupRouter()
 
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/api/v1/hello", nil)
+	req, _ := http.NewRequest(http.MethodGet, "/api/v1/hello", nil)
 	router.ServeHTTP(w, req)
 
 	assert.Equal(t, 200, w.Code)
